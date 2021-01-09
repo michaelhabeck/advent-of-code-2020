71d7b8236af5de9af40efcadf00c8ef45179a6a4
michaelhabeck <habeckhabeck@googlemail.com>
2021-01-09 02:01:06 +0100

    using counter

--- day10v2.py
+++ day10v2.py
@@ -1,5 +1,5 @@
 import functools
-from collections import defaultdict
+import collections
 
 def load_adapters(filename):
     adapters = list(sorted(map(int, open(filename).readlines())))
@@ -11,25 +11,24 @@ def n_paths(n):
     """
     a, b, c = 0, 0, 1
     for _ in range(n-1):
-        a, b, c = b, c, a+b+c
+        a, b, c = b, c, a + b + c
     return c
 
 if __name__ == '__main__':
         
     filename = 'input10.txt'
     adapters = load_adapters(filename)
+    jolts = [b-a for a, b in zip(adapters, adapters[1:])]
     
     # part 1
-    counts = defaultdict(int)
-    for a, b in zip(adapters, adapters[1:]):
-        counts[b-a] += 1
-    print('part 1:', counts[1] * counts[3])
+    counter = collections.Counter(jolts)
+    print('part 1:', counter[1] * counter[3])
 
     # part 2
     counts, n = 1, 0
-    for a, b in zip(adapters, adapters[1:]):
+    for jolt in jolts:
         n += 1
-        if b-a == 3:
+        if jolt == 3:
             counts *= n_paths(n)
             n = 0
     print('part 2:', counts)
